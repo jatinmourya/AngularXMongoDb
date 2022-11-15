95743707f0ac020a7bff927d27fe67030a7b6134
jatinmourya <jatinmourya07798@gmail.com>
2022-11-15 14:40:40 +0530

    15-11-22

--- backend/main.js
+++ backend/main.js
@@ -14,9 +14,9 @@ app.use(cors());
 
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-// url = "mongodb://localhost:27017/test";
-url =
-  "mongodb+srv://jatinmourya:jatinmourya@cluster0.tattcnd.mongodb.net/?retryWrites=true&w=majority";
+url = "mongodb://localhost:27017/";
+// url =
+  // "mongodb+srv://jatinmourya:jatinmourya@cluster0.tattcnd.mongodb.net/?retryWrites=true&w=majority";
 
 // connectURL = `${url}/${dbName}`;
 mongoose.connect(url, {
